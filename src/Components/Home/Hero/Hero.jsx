76740e677fc7bc2a5064e6e45eb88a42e3eaf165
redhumanidadesdigitales-sys <redhumanidadesdigitales@gmail.com>
2026-08-25 23:58:33 -0600

    Update Hero.jsx
    Actualización de fechas para la SemanaHD 2026

--- src/Components/Home/Hero/Hero.jsx
+++ src/Components/Home/Hero/Hero.jsx
@@ -14,10 +14,10 @@ const Hero = () => {
         </div>
         <div className="mt-10 lg:mt-0 lg:w-1/2 p-4 2xl:pl-10 text-center">
           <p className="mb-12 md:text-lg 2xl:text-2xl leading-relaxed 2xl:leading-10">
-          La <a href="https://twitter.com/search?q=%23SemanaHD&src=typed_query" target="_blank" rel="noopener noreferrer">#SemanaHD</a> es un evento abierto, gratuito y colaborativo  que se propone como un espacio en línea para dar a conocer proyectos  e iniciativas de Humanidades digitales en América Latina y el Caribe.  Si tienes un evento relacionado con las humanidades digitales (charlas, paneles, mesas de debates y talleres, etcétera) que ocurra entre el 3 y el 9 de noviembre, te invitamos a compartirlo y visibilizarlo en esta plataforma.</p>
+          La <a href="https://twitter.com/search?q=%23SemanaHD&src=typed_query" target="_blank" rel="noopener noreferrer">#SemanaHD</a> es un evento abierto, gratuito y colaborativo  que se propone como un espacio en línea para dar a conocer proyectos  e iniciativas de Humanidades digitales en América Latina y el Caribe.  Si tienes un evento relacionado con las humanidades digitales (charlas, paneles, mesas de debates y talleres, etcétera) que ocurra entre el 12 y el 16 de octubre, te invitamos a compartirlo y visibilizarlo en esta plataforma.</p>
           <ColorBar />
           <h4 className="mt-12 text-green_hd font-semibold text-xl md:text-2xl xl:text-3xl">
-            del 3 al 9 de noviembre de 2026
+            del 12 al 16 de octubre de 2026
           </h4>
         </div>
       </div>
